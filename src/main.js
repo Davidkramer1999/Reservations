d893ad9c92d615ac79a8f0e4e26d5401924d8397
David Kramer <david.kramer@itstudio.si>
2022-10-31 13:41:16 +0100

    adding boostrap

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import App from './App.vue'
 import router from './router'
 import VueGoodTablePlugin from 'vue-good-table';
 import 'vue-good-table/dist/vue-good-table.css'
-import './assets/main.css'
-
+// import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 
 const app = createApp(App)
--- src/views/AccommodationView.vue
+++ src/views/AccommodationView.vue
@@ -1,35 +1,38 @@
 <template>
-    <ag-grid-vue style="width: 500px; height: 200px" class="ag-theme-alpine" :columnDefs="columnDefs"
-        :rowData="rowData">
-    </ag-grid-vue>
+    <div>
+        <b-table striped hover :items="items" :fields="fields"></b-table>
+    </div>
 </template>
   
 <script>
-import "ag-grid-community/styles//ag-grid.css";
-import "ag-grid-community/styles//ag-theme-alpine.css";
-import { AgGridVue } from "ag-grid-vue3";
-
 export default {
-    name: "App",
-    components: {
-        AgGridVue,
-    },
     data() {
         return {
-            columnDefs: [
-                { headerName: "Make", field: "id" },
-                { headerName: "Model", field: "createdAt" },
-                { headerName: "Price", field: "price" },
+            // Note 'isActive' is left out and will not appear in the rendered table
+            fields: [
+                {
+                    key: 'last_name',
+                    sortable: true
+                },
+                {
+                    key: 'first_name',
+                    sortable: false
+                },
+                {
+                    key: 'age',
+                    label: 'Person age',
+                    sortable: true,
+                    // Variant applies to the whole column, including the header and footer
+                    variant: 'danger'
+                }
             ],
-            rowData: [],
-        };
-
-    },
-    async mounted() {
-        const response = await fetch("https://5ddbbbd5041ac10014de14d7.mockapi.io/accommodations/prices");
-        const data = await response.json()
-        this.rowData = data
-        console.warn(this.rowData)
-    },
-};
+            items: [
+                { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+                { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+                { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+                { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
+            ]
+        }
+    }
+}
 </script>
